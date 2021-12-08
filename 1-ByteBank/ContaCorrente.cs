d781de6a2d1f4c9f0cc22791a104b9aad5f4cf32
vitormendes4072 <vitormendes4072@gmail.com>
2021-12-08 12:09:21 -0300

    Add project files.

---
+++ 1-ByteBank/ContaCorrente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_ByteBank
+{
+    class ContaCorrente
+    {
+        public string Titular { get; set; }
+        public int NumeroAgencia { get; set; }
+        public int NumeroConta { get; set; }
+        public double Saldo { get; set; }
+    }
+}
---
+++ 1-ByteBank/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente vitoria = new ContaCorrente();
+            vitoria.Saldo = 10;
+            Console.WriteLine(vitoria.Saldo);
+        }
+    }
+}
