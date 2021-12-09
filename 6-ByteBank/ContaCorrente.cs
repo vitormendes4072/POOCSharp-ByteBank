4f19616279d0ae6b10e2e0cea246a13453dec44f
vitormendes4072 <vitormendes4072@gmail.com>
2021-12-09 20:44:32 -0300

    Visibilidade dos atributos, get e set

---
+++ 6-ByteBank/ContaCorrente.cs
@@ -0,0 +1,42 @@
+﻿using _6_ByteBank;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6_ByteBank
+{
+    class ContaCorrente
+    {
+        public int NumeroAgencia { get; set; }
+        public int NumeroConta { get; set; }
+        public Cliente Titular {get; set;}
+        private double _saldo;
+
+        public double Saldo
+        {
+            get { return _saldo; }
+            set { if (value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (Saldo < valor)
+            {
+                return false;
+            }
+            Saldo -= valor;
+            return true;
+        }
+
+        public void Transferir(double valor, ContaCorrente conta)
+        {
+            Sacar(valor);
+            conta.Saldo = valor;
+        }
+    }
+}
---
+++ 6-ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            conta.Saldo = -10;
+            conta.Titular = new Cliente();
+            conta.Titular.Nome = "Felipe";
+            conta.Titular.Cpf = "235.467.577-57";
+            conta.Titular.Profissao = "Desenvolvedor Java";
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+}
